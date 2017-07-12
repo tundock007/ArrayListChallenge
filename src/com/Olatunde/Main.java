88862a9d5ff486d315ce28c46b3c1272e4498095
Ola <tundock007@gmail.com>
2017-07-11 20:19:45 -0700

    Initial commit

---
+++ src/com/Olatunde/Main.java
@@ -0,0 +1,11 @@
+package com.Olatunde;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+    }
+}
---
+++ src/com/Olatunde/MobilePhone.java
@@ -0,0 +1,25 @@
+package com.Olatunde;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tundo on 7/11/2017.
+ */
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        if(findContact(contact.getName()) >=0 ) {
+            System.out.println("Contact is already on file");
+            return  false;
+        }
+
+        myContacts.add(contact);
+    }
+}
